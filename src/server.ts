e06e36fea97f65eded1c7312416ab4f797386929
Luiz Rogerio <lrbengozi@gmail.com>
2021-06-24 09:33:31 -0300

    Create a CORS

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,15 @@ import "./database";
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use(router);
